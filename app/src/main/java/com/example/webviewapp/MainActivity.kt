f9bf74166fd2147aa010509be1c2bf62571b7b89
Marcel Pernia <marcelpm1987@gmail.com>
2025-05-14 17:01:52 +0200

    upload directory app

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.webviewapp
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        
+        webView.loadUrl("https://biserca.app")
+    }
+
+    override fun onBackPressed() {
+        val webView = findViewById<WebView>(R.id.webView)
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+} 
